dd73c7b9b10a25879e96dacd5921aeddab83ef00
Chu <liangforcoding@gmail.com>
2025-07-21 01:17:42 +0800

    variable

---
+++ 2_variable/main.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    // int (-2,147,483,648 to 2,147,483,647)
+    int i = 10;
+    std::cout << "i = " << i << std::endl;
+
+    int i2 = 5.5;
+    std::cout << "i2 = " << i2 << std::endl;
+
+    // short (-32,768 to 32,767)
+    short s = 10;
+    std::cout << "s = " << s << std::endl;
+
+    short s2 = 32767 + 1; //overflow
+    std::cout << "s = " << s2 << std::endl;
+
+    // long long (-9,223,372,036,854,775,808 to 9,223,372,036,854,775,807)
+    long long ll = 123;
+    std::cout << "ll = " << ll << std::endl;
+
+    
+    std::cout << "----------" << std::endl;
+
+    // float
+    float f = 5;
+    std::cout << "f = " << f << std::endl;
+
+    float f2 = 1.5;
+    std::cout << "f2 = " << f2 << std::endl;
+
+    std::cout << "----------" << std::endl;
+
+    // char
+    char c = 'A';
+    std::cout << "c = " << c << std::endl;
+
+    char c2 = 'A' + 1;
+    std::cout << "c2 = " << c2 << std::endl;
+
+    char c3 = 65;
+    std::cout << "c3 = " << c3 << std::endl;
+
+    std::cout << "----------" << std::endl;
+
+    // string
+    std::string str = "hello";
+    std::cout << "str = " << str << std::endl;
+
+    std::string str2 = "world";
+    std::cout << "str2 = " << str2 << std::endl;
+
+    std::cout << "str + str2 = " << str + " " + str2 << std::endl;
+
+    std::cout << "----------" << std::endl;
+
+    // bool
+    bool b = true;
+    std::cout << "b = " << b << std::endl;
+
+    bool b2 = false;
+    std::cout << "b2 = " << b2 << std::endl;
+
+    bool b3 = 10;
+    std::cout << "b3 = " << b3 << std::endl;
+
+    std::cout << "----------" << std::endl;
+
+    // int (-2,147,483,648 to 2,147,483,647)
+    // unsigned int (0 to 4,294,967,295)
+    unsigned int ui = -1;
+    std::cout << "ui = " << ui << std::endl;
+
+    // short (-32,768 to 32,767)
+    // unsigned short (0 to 65,535)
+    unsigned short us = 65535 + 1;
+    std::cout << "us = " << us << std::endl;
+
+    // long long (-9,223,372,036,854,775,808 to 9,223,372,036,854,775,807)
+    // unsigned long long (0 to 18,446,744,073,709,551,615)
+    unsigned long long ull = -1;
+    std::cout << "ull = " << ull << std::endl;
+
+    return 0;
+}
